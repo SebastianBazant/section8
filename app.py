aec3cdff91f3a92082f03d2ac5429530ad4a76a8
unknown <sebazant@gmail.com>
2021-11-24 00:39:28 +0100

    Change in resources/item, resources/user, requirements, model/item, security, app

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 from db import db
 
-from security import authenticate, identity
-from resources.user import UserRegister, User
+
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 
 from resources.store import Store, StoreList
@@ -19,7 +19,7 @@ api = Api(app)
 app.secret_key = 'jose'
 
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWTManager(app)
 
 api.add_resource(Store, "/store/<string:name>")
 api.add_resource(Item, "/item/<string:name>")
@@ -27,6 +27,7 @@ api.add_resource(ItemList, "/items")
 api.add_resource(StoreList, "/stores")
 api.add_resource(UserRegister, "/register")
 api.add_resource(User, "/user/<int:user_id>")
+api.add_resource(UserLogin, "/login")
 if __name__ == "__main__":
 
     db.init_app(app)
--- resources/user.py
+++ resources/user.py
@@ -2,20 +2,23 @@ import sqlite3
 
 from flask_restful import reqparse, Resource
 from models.user import UserModel
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token, create_refresh_token
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username', required=True,
+                          type=str,
+                          help="Username cannot be blank!")
+
+_user_parser.add_argument('password', required=True,
+                          type=str,
+                          help="Password cannot be blank!")
 
 
 class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username', required=True,
-                        type=str,
-                        help="Username cannot be blank!")
-
-    parser.add_argument('password', required=True,
-                        type=str,
-                        help="Password cannot be blank!")
 
     def post(self):
-        data = UserRegister.parser.parse_args()
+        data = _user_parser.parse_args()
 
         if UserModel.find_by_username(data["username"]):
             return {"message": "A user with that username already exists"}, 400
@@ -41,3 +44,26 @@ class User(Resource):
             return{"message": "User not found"}, 404
         user.delete_from_db()
         return{"message": "User deleted"}, 200
+
+
+class UserLogin(Resource):
+
+    def post(cls):
+        # get data from parser
+        data = _user_parser.parse_args()
+        # find user in dabase
+        user = UserModel.find_by_username(data["username"])
+        # check password
+        if user and safe_str_cmp(user.password, data["password"]):
+            access_token = create_access_token(identy=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+
+            return{
+                "access_token": access_token,
+                "refresh_token": refresh_token
+
+            }, 200
+
+        return {"message": "Invalid credentials"}, 401
+        # create access token
+        # create refresh token
